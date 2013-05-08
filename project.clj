b007e09c5bf136b393f6a4604a6ead92bf84c62b
owain lewis <owain@owainlewis.com>
2013-05-08 10:19:58 +0100

    bumps version for release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject salesforce "0.1.2-SNAPSHOT"
+(defproject salesforce "0.1.3-SNAPSHOT"
   :description "A clojure library for accessing the salesforce api"
-  :url "http://owainlewis.com"
+  :url "http://boxuk.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
