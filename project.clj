e9c396278d2579c5df123315e52b6e2072d63c47
Kendall Buchanan <kendall@kendagriff.com>
2016-02-02 15:36:31 -0700

    bump clj-http version

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "0.7.2"]
+                 [clj-http "2.0.1"]
                  [cheshire "5.1.1"]])
