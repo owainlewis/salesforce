b7568174fdbeca1f67b37ab0fad2a8f83dc65149
Kendall Buchanan <kendall@kendagriff.com>
2016-02-02 15:38:36 -0700

    bump clojure ver

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject salesforce "1.0.1"
+(defproject com.banzai/salesforce "1.0.6"
   :description "A clojure library for accessing the salesforce api"
   :url "http://owainlewis.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "2.0.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "0.7.2"]
                  [cheshire "5.1.1"]])
